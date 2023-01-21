68859d8fd4bf6bdf4d90dafb38885716d3f6cb65
JuliansRecent <juliansrecent@gmail.com>
2023-01-20 16:32:53 -0800

    Module complete. Prompt the user in console for elements of a story character. Print a story to the console using the users inputs.

--- src/main/java/Story.java
+++ src/main/java/Story.java
@@ -7,6 +7,12 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("I will tell you a story, but I need some information first.\nWhat is the main character called?");
+        String character_name = scanner.nextLine();
+        System.out.println("What is their job?");
+        String character_job = scanner.nextLine();
+        System.out.println("Here is the story:\nOnce upon a time there was " + character_name + ", who was " + character_job
+                + ".\nOn the way to work, " + character_name + " reflected on life.\nPerhaps "
+                + character_name + " will not be " + character_job + " forever.");
     }
 }
